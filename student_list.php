e3489719990f1dbe93204700e22715af129290db
ashikhassan <ashik7raj@gmail.com>
2014-12-07 10:59:14 +0600

    Update some syntax error.

--- student_list.php
+++ student_list.php
@@ -1,5 +1,6 @@
-<?php session_start(); $student_id="";
-$student_id=count($_SESSION['students']);
+<?php session_start();
+//$student_id="";
+//$student_id=count($_SESSION['students']);
 //echo "<pre>";
 //print_r($_SERVER);
 //print_r($_SESSION);
